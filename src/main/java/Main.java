b5773ee3aab3f33ab46fbc58477d6c6e8277c3cd
tatiSmol <judeclapton@icloud.com>
2023-11-10 00:07:24 +0300

    Configuration preparation

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory factory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = factory.openSession();
+
+
+        factory.close();
+    }
+}
